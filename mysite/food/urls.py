1a71c5d66171223ed10701cc9947f925ead49831
sumantaman <161899459+sumantaman@users.noreply.github.com>
2025-06-29 18:12:32 +0545

    class based : detail views

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
    #  food
    path('',views.IndexClassView.as_view(),name = 'index'),
    # food/1
-   path('<int:item_id>/', views.detail, name = 'detail'),
+   path('<int:pk>/', views.FoodDetails.as_view(), name = 'detail'),
    path('item/', views.item, name = 'item'),
    # add
    path('add/', views.create_item, name='create_item'),
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -4,14 +4,17 @@ from .models import Item
 from django.template import loader
 from .forms import ItemForm
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
-def index(request):
-    item_list = Item.objects.all()
-    context = {
-        'item_list': item_list,
-    }
-    return render(request, 'food/index.html', context)
+# function based vieww
+# def index(request):
+#     item_list = Item.objects.all()
+#     context = {
+#         'item_list': item_list,
+#     }
+#     return render(request, 'food/index.html', context)
 
+# class based views and list views
 class IndexClassView(ListView):
     model = Item;
     template_name ='food/index.html'
@@ -21,12 +24,12 @@ class IndexClassView(ListView):
 def item(resquest):
     return HttpResponse("item page")
 
-def detail(request,item_id):
-    item = Item.objects.get(pk = item_id)
-    context ={
-        'item': item,
-    }
-    return render(request, 'food/detail.html',context)
+
+# detail views
+class FoodDetails(DetailView):
+    model = Item;
+    template_name ='food/detail.html'
+
 
 
 def create_item(request):
